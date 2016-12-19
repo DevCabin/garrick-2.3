10fba5fac9d3c49d20a05253e790ff134e891b99
George Featherstone <DevCabin@users.noreply.github.com>
2016-12-19 08:38:57 -0600

    Remove GA

--- footer.php
+++ footer.php
@@ -90,20 +90,6 @@ if ( function_exists( 'ot_get_option' ) ) {
 
 <?php wp_footer(); ?>
 
-<!-- analytics -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-82450298-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-<!-- / analytics -->
-
-
 <?php include('growthpop.php');?>
 
 </body>
